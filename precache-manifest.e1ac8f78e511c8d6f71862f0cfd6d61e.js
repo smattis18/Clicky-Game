74efd95ea2f62bb186be7311cdddc3ed049412ed
Steve Mattis <smattis@cfl.rr.com>
2019-02-12 20:35:29 -0500

    Updates

---
+++ precache-manifest.e1ac8f78e511c8d6f71862f0cfd6d61e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "caa28073e1558e3d74e6",
+    "url": "/Clicky-Game/static/js/main.caa28073.chunk.js"
+  },
+  {
+    "revision": "0a7a1053aaedd785abb5",
+    "url": "/Clicky-Game/static/js/1.0a7a1053.chunk.js"
+  },
+  {
+    "revision": "caa28073e1558e3d74e6",
+    "url": "/Clicky-Game/static/css/main.51c40b74.chunk.css"
+  },
+  {
+    "revision": "3fc495203ab4fd93d0a416d7844229c0",
+    "url": "/Clicky-Game/index.html"
+  }
+];
